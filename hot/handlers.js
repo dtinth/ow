f29aab2065928e75a8b2baa02a07f0c5e4e940dd
Glitch (hello-express) <dtinth@spacet.me>
2020-10-19 16:48:31 +0000

    🎀🍅 Checkpoint ./hot/handlers.js:577339/883 ./public/README.md:577339/407

--- hot/handlers.js
+++ hot/handlers.js
@@ -3,8 +3,24 @@ exports.onMessage = async (message, { client }) => {
     return
   }
   if (!message.member) {
-    message.reply('For information on how to use this bot, please check its documentation: https://ow-discord-bot.glitch.me/')
+    message.reply('For information on how to use this bot, please check its documentation: https://ow.wonderful.software/')
     return
   }
-  message.reply('ugh')
+  const prefix = '///'
+  if (typeof message.content !== 'string') {
+    return
+  }
+  if (!message.content.startsWith(prefix)) {
+    return
+  }
+
+  const parts = message.content.substr(prefix.length).split(/\s+/)
+  if (parts[0] === 'ow') {
+    if (parts[1] === 'register') {
+      message.reply('Register!')
+      return
+    }
+    message.reply('Unrecognized command. See documentation: https://ow.wonderful.software/')
+    return
+  }
 }
--- public/README.md
+++ public/README.md
@@ -1,7 +1,7 @@
 ow Discord Bot
 ==============
 
-**ow Discord Bot** is a simple Discord bot that lets everyone in your server create a custom command using a simple HTTP endpoint.
+**ow Discord Bot** is a simple Discord bot that lets _everyone_ in your server create a custom Discord commands using a simple HTTP endpoint.
 
 ## Usage reference
 
@@ -19,6 +19,17 @@ First, [add the bot to your server][add].
 
 - `<command-name>` Name of the command.
 - `<url>` URL to run.
+
+### Handing requests from `ow`
+
+`ow` will send a POST request to your registered endpoint.
+To verify the authenticity of the request, please check the `authorization` header, which is in this form:
+
+```
+authorization: bearer <jwt>
+```
+
+- `<jwt>` A JWT token 
 
 ## About this bot
 
